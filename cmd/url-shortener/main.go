2cf972e24857120f4ed54052f6b7040e7b81dda6
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2023-07-18 15:20:45 +0300

    config.go

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// TODO: inti logger: slog
+
+	// TODO: inti storage: sqlite
+
+	// TODO: inti router: chi, "chi reder"
+
+	// TODO: run server
+}
