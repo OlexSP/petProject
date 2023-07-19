9693c92ec49d1f3e3e5e5901fe926c978fd3fb62
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2023-07-19 18:41:30 +0300

    logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"golang.org/x/exp/slog"
+	"os"
 	"petProject/internal/config"
 )
 
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
+
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
 
-	// TODO: inti logger: slog
+	log.Info("starting url-shortener")
 
 	// TODO: inti storage: sqlite
 
 	// TODO: inti router: chi, "chi reder"
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
